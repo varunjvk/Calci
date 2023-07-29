013e0f360aad241033e43e6d4fbda9b63ad84952
Varun <jvkumar560@outlook.com>
2023-07-29 16:05:19 +0530

    first commit

---
+++ src/calci.js
@@ -0,0 +1,8 @@
+let calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const pro = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return { add, sub, pro, div };
+})();
+
